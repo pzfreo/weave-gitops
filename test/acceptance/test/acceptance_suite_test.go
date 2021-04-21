c37543ca7d3679e9e462ea42c958f9cee3dd5f4a
Iftikhar Ahmad <iftikhar@weave.works>
2021-04-21 17:36:15 +0100

    set default timeout for eventually

--- test/acceptance/test/acceptance_suite_test.go
+++ test/acceptance/test/acceptance_suite_test.go
@@ -19,9 +19,13 @@ func TestAcceptance(t *testing.T) {
 
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Test Suite")
+
 }
 
 var _ = BeforeSuite(func() {
+
+	SetDefaultEventuallyTimeout(EVENTUALLY_DEFAULT_TIME_OUT)
+
 	WEGO_BIN_PATH = os.Getenv("WEGO_BIN_PATH")
 	if WEGO_BIN_PATH == "" {
 		WEGO_BIN_PATH = "/usr/local/bin/wego"
